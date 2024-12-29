c707dff91f8870981f6c484a4dc802cd96cbd040
fender-git <roel.vanransbeke@gmail.com>
2024-12-29 17:34:23 +0100

    sorted characters - only letters

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
-    report = get_report(book_path, num_words, num_characters)
+    list_characters = get_list_characters(num_characters)
+    report = get_report(book_path, num_words, list_characters)
     return report
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 def get_num_characters(text):
     lowered_text = text.lower()
@@ -14,18 +21,24 @@ def get_num_characters(text):
             overview[character] += 1
         else:
             overview[character] = 1
-    return overview
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    return(overview)
+    
+    
+def get_list_characters(num_characters):
+    list_of_dicts = [{"key": key, "count": count} for key, count in num_characters.items()]
+    sorted_list = sorted(list_of_dicts, key=lambda x: x['count'], reverse=True)
+    letters_only = [entry for entry in sorted_list if entry['key'].isalpha()]
+    return letters_only
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_report(book_path, num_words, num_characters):
+def get_report(book_path, num_words, list_characters):
     print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+    print(f"{num_words} words found in the document"
+          )
+    print(list_characters)
+    print("--- End report ---")
 
 main()
